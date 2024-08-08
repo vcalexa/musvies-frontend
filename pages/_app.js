203e6b885e2d181a5940e73d041a8e9c975ccdb3
W <gg>
2024-08-08 10:53:43 +0200

    remove secrets

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <GoogleOAuthProvider clientId="976216336745-6chr0tn4sevdkpsub66sp0i0nrjk0ct9.apps.googleusercontent.com">
-      <Component {...pageProps} />
-    </GoogleOAuthProvider>
   );
 };
 
